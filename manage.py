bea6a13b610ff18749c1656bde6f18146589a356
gcmestre <goncalo@lablinks.io>
2026-03-04 14:16:28 +0000

    ruff formated

--- manage.py
+++ manage.py
@@ -6,9 +6,7 @@ import sys
 
 
 def main():
-    settings_module = (
-        "config.settings.prod" if "WEBSITE_HOSTNAME" in os.environ else "config.settings.dev"
-    )
+    settings_module = "config.settings.prod" if "WEBSITE_HOSTNAME" in os.environ else "config.settings.dev"
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
     try:
         from django.core.management import execute_from_command_line
